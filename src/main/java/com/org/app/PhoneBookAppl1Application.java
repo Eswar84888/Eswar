11defede174281ce42943221ee4f86ef0a52d1ff
unknown <eswar84888@gmail.com>
2022-03-07 22:47:17 +0530

    first comment

---
+++ src/main/java/com/org/app/PhoneBookAppl1Application.java
@@ -0,0 +1,13 @@
+package com.org.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookAppl1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookAppl1Application.class, args);
+	}
+
+}
